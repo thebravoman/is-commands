64984203a5fa0716bac210700a1e851d6fae5836
kireto <k.mitov@robopartans.com>
2020-01-31 08:37:05 +0200

    v1.0.6 [Version] 1.0.6 Autopretifying on build

--- app/assets/javascripts/ext/commands/executed_event.js
+++ app/assets/javascripts/ext/commands/executed_event.js
@@ -9,39 +9,37 @@
  * @author Kiril Mitov
  */
 IS.Commands.ExecutedEvent = class {
+  /**
+   * @param {Array<IS.Commands.StepCommand>} commands list of commands for this event in order that they are applied
+   * @param {string} action the action of execute/undo/redo that was done on the commands in this event.
+   */
+  constructor(commands, action) {
+    /**
+     * @private
+     * @type {Array<IS.Commands.StepCommand>}
+     */
+    this._commands = commands;
 
-	/**
-	 * @param {Array<IS.Commands.StepCommand>} commands list of commands for this event in order that they are applied
-	 * @param {string} action the action of execute/undo/redo that was done on the commands in this event.
-	 */
-	constructor(commands,action) {
-		/**
-		 * @private
-		 * @type {Array<IS.Commands.StepCommand>}
-		 */
-		this._commands = commands
+    /**
+     * @private
+     * @type {string}
+     */
+    this._action = action;
+  }
 
-		/**
-		 * @private
-		 * @type {string}
-		 */
-		this._action = action
-	}
+  /**
+   * @export
+   * @return {Array<IS.Commands.StepCommand>} array of commands store in this event
+   */
+  getCommands() {
+    return this._commands;
+  }
 
-	/**
-	 * @export
-	 * @return {Array<IS.Commands.StepCommand>} array of commands store in this event
-	 */
-	getCommands() {
-		return this._commands
-	}
-
-	/**
-	 * @export
-	 * @return {string}
-	 */
-	getAction() {
-		return this._action;
-	}
-	
-}
+  /**
+   * @export
+   * @return {string}
+   */
+  getAction() {
+    return this._action;
+  }
+};
--- test/dummy/spec/javascripts/ext/commands/history_spec.js
+++ test/dummy/spec/javascripts/ext/commands/history_spec.js
@@ -3,161 +3,161 @@
 //= require ext/commands/step_command
 //= require ext/commands/executed_event
 describe("IS.Commands.History", function() {
-	
-	class DummyCommand extends IS.Commands.StepCommand {
+  class DummyCommand extends IS.Commands.StepCommand {
+    constructor() {
+      super();
+    }
 
-		constructor(){ 
-			super();
-		}
+    execute() {
+      this.executed = DummyCommand.ORDER++;
+    }
+    undo() {
+      this.undone = DummyCommand.ORDER++;
+    }
+    redo() {
+      this.redone = DummyCommand.ORDER++;
+    }
+  }
+  DummyCommand.ORDER = 0;
 
-		execute() {
-			this.executed = DummyCommand.ORDER++;
-		}
-		undo() {
-			this.undone = DummyCommand.ORDER++;
-		}
-		redo() {
-			this.redone = DummyCommand.ORDER++;
-		}
-	}
-	DummyCommand.ORDER = 0;
+  class DummyListener extends IS.Extension {
+    constructor() {
+      super();
+    }
 
-	class DummyListener extends IS.Extension {
-		constructor() {
-			super();
-		}
+    onCommandsExecuted(event) {
+      this.commandsExecuted = event;
+    }
+  }
 
-		onCommandsExecuted(event) {
-			this.commandsExecuted = event;
-		}
+  beforeEach(function() {
+    this.commandsHistory = new IS.Commands.History();
+    this.dummyListener = new DummyListener();
+    const ext = [
+      IS.ExtensionDef.CreateByExtensionConf(
+        {
+          "extension": this.commandsHistory,
+          "events": {
+            "commandsExecuted": {
+              "class": Object
+            }
+          }
+        },
+        "dev"
+      ),
+      IS.ExtensionDef.CreateByExtensionConf(
+        {
+          "extension": this.dummyListener,
+          "events": {
+            "commandsExecuted": {
+              "receivesWith": this.dummyListener.onCommandsExecuted
+            }
+          }
+        },
+        "dev"
+      )
+    ];
+    this.isCore = new IS.Core(ext);
+    this.isCore.init();
 
-	}
+    this.command1 = new DummyCommand();
+    this.command2 = new DummyCommand();
+    DummyCommand.ORDER = 0;
+  });
 
-	beforeEach(function() {
-		this.commandsHistory = new IS.Commands.History();
-		this.dummyListener = new DummyListener();
-		const ext = [
-			IS.ExtensionDef.CreateByExtensionConf({
-				"extension": this.commandsHistory,
-				"events": {
-					"commandsExecuted": {
-						"class": Object
-					}
-				}
-			}, "dev"),
-			IS.ExtensionDef.CreateByExtensionConf({
-				"extension": this.dummyListener,
-				"events": {
-					"commandsExecuted": {
-						"receivesWith": this.dummyListener.onCommandsExecuted 
-					} 
-				}
-			}, "dev")
-		]
-		this.isCore = new IS.Core(ext);
-		this.isCore.init();
+  it("initializes position in the init method and not in the constructor because if in the constructor an error occurs that position is NAN. Don't know why yet", function() {
+    const ch = new IS.Commands.History();
+    expect(ch._position).toEqual(undefined);
+    ch.init(null, null);
+    expect(ch._position).toEqual(0);
+  });
 
-		this.command1 = new DummyCommand();
-		this.command2 = new DummyCommand();
-		DummyCommand.ORDER = 0;
-	})
+  describe("execute", function() {
+    beforeEach(function() {
+      this.commandsHistory.execute([this.command1, this.command2]);
+    });
 
-	it("initializes position in the init method and not in the constructor because if in the constructor an error occurs that position is NAN. Don't know why yet", function(){
-		const ch = new IS.Commands.History();
-		expect(ch._position).toEqual(undefined)
-		ch.init(null,null)
-		expect(ch._position).toEqual(0)
-	})
+    it("executed two commands on execute", function() {
+      expect(this.command1.executed).toEqual(0);
+      expect(this.command2.executed).toEqual(1);
+    });
 
-	describe("execute", function(){
-		beforeEach(function() {
-			this.commandsHistory.execute([this.command1, this.command2])
-		})
+    it("schedules a notification with executed to listeners", function() {
+      expect(this.dummyListener.commandsExecuted.getAction()).toEqual("execute");
+    });
 
-		it("executed two commands on execute", function() {
-			expect(this.command1.executed).toEqual(0);
-			expect(this.command2.executed).toEqual(1);
-		})
+    it("increases undo count", function() {
+      expect(this.commandsHistory.getUndoCount()).toEqual(1);
+    });
 
-		it("schedules a notification with executed to listeners", function() {
-			expect(this.dummyListener.commandsExecuted.getAction()).toEqual("execute")
-		})
+    it("replaces the undone commands with the second commands and the undone can no longer be redone", function() {
+      const command3 = new DummyCommand();
+      this.commandsHistory.undo();
+      this.commandsHistory.execute([command3]);
 
-		it("increases undo count",function() {
-			expect(this.commandsHistory.getUndoCount()).toEqual(1)
-		});
+      expect(this.commandsHistory.getUndoCount()).toEqual(1);
+      expect(this.commandsHistory.getRedoCount()).toEqual(0);
+      expect(command3.executed).toEqual(4);
+      this.commandsHistory.undo();
+      expect(command3.undone).toEqual(5);
+    });
+  });
 
-		it("replaces the undone commands with the second commands and the undone can no longer be redone", function(){
-			const command3 = new DummyCommand();
-			this.commandsHistory.undo();
-			this.commandsHistory.execute([command3])
+  describe("undo", function() {
+    beforeEach(function() {
+      this.commandsHistory.execute([this.command1, this.command2]);
+      this.commandsHistory.undo();
+    });
 
-			expect(this.commandsHistory.getUndoCount()).toEqual(1)
-			expect(this.commandsHistory.getRedoCount()).toEqual(0)
-			expect(command3.executed).toEqual(4)
-			this.commandsHistory.undo();
-			expect(command3.undone).toEqual(5)
-		});
+    it("undones all executed commands on previous steps", function() {
+      expect(this.command1.undone).toEqual(3);
+      expect(this.command2.undone).toEqual(2);
+    });
 
-	})
+    it("schedules a notification with undo to listeners", function() {
+      expect(this.dummyListener.commandsExecuted.getAction()).toEqual("undo");
+    });
 
-	describe("undo", function() {
+    it("descrease undo count", function() {
+      expect(this.commandsHistory.getUndoCount()).toEqual(0);
+    });
 
-		beforeEach(function() {
-			this.commandsHistory.execute([this.command1, this.command2])
-			this.commandsHistory.undo();
-		})
+    it("increases redo count", function() {
+      expect(this.commandsHistory.getRedoCount()).toEqual(1);
+    });
 
-		it("undones all executed commands on previous steps",function() {
-			expect(this.command1.undone).toEqual(3)
-			expect(this.command2.undone).toEqual(2)
-		})
+    it("does nothing if there is nothing to undo", function() {
+      this.commandsHistory.undo();
+    });
+  });
 
-		it("schedules a notification with undo to listeners", function() {
-			expect(this.dummyListener.commandsExecuted.getAction()).toEqual("undo")
-		})
-		
-		it("descrease undo count", function() {
-			expect(this.commandsHistory.getUndoCount()).toEqual(0)
-		});
+  describe("redo", function() {
+    beforeEach(function() {
+      this.commandsHistory.execute([this.command1, this.command2]);
+      this.commandsHistory.undo();
+      this.commandsHistory.redo();
+    });
 
-		it("increases redo count", function() {
-			expect(this.commandsHistory.getRedoCount()).toEqual(1)
-		})
+    it("redones all undone commands on previous steps", function() {
+      expect(this.command1.redone).toEqual(4);
+      expect(this.command2.redone).toEqual(5);
+    });
 
-		it("does nothing if there is nothing to undo", function(){
-			this.commandsHistory.undo();
-		})
-	})
+    it("schedules a notification with undo to listeners", function() {
+      expect(this.dummyListener.commandsExecuted.getAction()).toEqual("redo");
+    });
 
-	describe("redo", function() {
-		beforeEach(function() {
-			this.commandsHistory.execute([this.command1, this.command2])
-			this.commandsHistory.undo();
-			this.commandsHistory.redo();
-		})
+    it("increases undo count", function() {
+      expect(this.commandsHistory.getUndoCount()).toEqual(1);
+    });
 
-		it("redones all undone commands on previous steps", function() {
-			expect(this.command1.redone).toEqual(4)
-			expect(this.command2.redone).toEqual(5)
-		})
+    it("descreases redo count", function() {
+      expect(this.commandsHistory.getRedoCount()).toEqual(0);
+    });
 
-		it("schedules a notification with undo to listeners", function() {
-			expect(this.dummyListener.commandsExecuted.getAction()).toEqual("redo")
-		})
-
-		it("increases undo count", function() {
-			expect(this.commandsHistory.getUndoCount()).toEqual(1)
-		})
-		
-		it("descreases redo count", function() {
-			expect(this.commandsHistory.getRedoCount()).toEqual(0)
-		})
-
-		it("does nothing if there is nothing to redo", function() {
-			expect(this.commandsHistory.getRedoCount()).toEqual(0)
-			this.commandsHistory.redo();
-		})
-	});
-
-})
+    it("does nothing if there is nothing to redo", function() {
+      expect(this.commandsHistory.getRedoCount()).toEqual(0);
+      this.commandsHistory.redo();
+    });
+  });
+});
