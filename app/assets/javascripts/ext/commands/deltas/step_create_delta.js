2addca9f2e69b8d0a2593258e299f06750903867
Dimitar Lukanov <d.lukanov@robopartans.com>
2020-01-31 15:33:38 +0200

    Added documentation and google closure compiler tags

--- app/assets/javascripts/ext/commands/deltas/step_create_delta.js
+++ app/assets/javascripts/ext/commands/deltas/step_create_delta.js
@@ -4,6 +4,12 @@
 /**
  * Delta that contains the changes made to a step via the {@link IS.Commands.StepCreateCommand}
  *
+ * The getStep, getParent, getPosition just have a different semantic meaning from the other deltas.
+ *
+ * - getStep() returns the step we've just created.
+ * - getParent() returns the new steps parent step.
+ * - getPosition() returns the position in which we put the new step in, in the children attribute of the parent step
+ *
  * @export
  * @author Dimitar Lukanov
  */
--- app/assets/javascripts/ext/commands/deltas/step_reorder_delta.js
+++ app/assets/javascripts/ext/commands/deltas/step_reorder_delta.js
@@ -4,6 +4,12 @@
 /**
  * Delta that contains the changes made to a step via the {@link IS.Commands.StepReorderCommand}.
  *
+ * The getStep, getParent, getPosition just have a different semantic meaning from the other deltas.
+ *
+ * - getStep() returns the step we've just moved.
+ * - getParent() returns the steps new parent step.
+ * - getPosition() returns the position in which we put the step in, in the children attribute of the new parent step
+ *
  * @export
  * @author Dimitar Lukanov
  */
